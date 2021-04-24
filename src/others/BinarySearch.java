02a8fba9fac1f36da0902120a3f778bbf3f6d140
Kenichi Yukihira <tyanyuki3.39@gmail.com>
2021-04-24 10:52:02 +0200

    modified binarySearch method so that you can use upper bound

--- src/others/BinarySearch.java
+++ src/others/BinarySearch.java
@@ -4,6 +4,45 @@ import java.util.*;
 
 public class BinarySearch {
 
+    // Binary search template
+    static int binarySearch(boolean isLower, int key) {
+        int ng; int ok;
+        if (isLower) {
+            // Lower bound
+            ng = -1;
+            ok = arr.size();
+        } else {
+            // Upper bound
+            ok = -1;
+            ng = arr.size();
+        }
+
+        // We don't know which is bigger
+        while (Math.abs(ok - ng) > 1) {
+            int mid = (ok + ng) / 2;
+
+            if (isOk(mid, isLower, key)) ok = mid;
+            else ng = mid;
+        }
+
+        if (isLower) return ok;
+        else return ng;
+    }
+
+    // Helper method for binarySearch
+    // Check if the index meets the condition
+    static boolean isOk(int index, boolean isLower, int key) {
+        if (isLower) {
+            // Lower bound
+            if (arr.get(index) >= key) return true;
+            else return false;
+        } else {
+            // Upper bound
+            if (arr.get(index) > key) return false;
+            else return true;
+        }
+    }
+
     // Comparator for searching lower bound
     // Make sure to negate the return value
     static class LowerBoundComparator<T extends Comparable<? super T>>
@@ -26,29 +65,6 @@ public class BinarySearch {
         }
     }
 
-    // Binary search template
-    static int binarySearch(int key) {
-        int ng = -1;
-        int ok = arr.size();
-
-        // We don't know which is bigger
-        while (Math.abs(ok - ng) > 1) {
-            int mid = (ok + ng) / 2;
-
-            if (isOk(mid, key)) ok = mid;
-            else ng = mid;
-        }
-        return ok;
-    }
-
-    // Helper method for binarySearch
-    // Check if the index meets the condition
-    static boolean isOk(int index, int key) {
-        //Lower bound
-        if (arr.get(index) >= key) return true;
-        else return false;
-    }
-
     static List<Integer> arr;
 
     public static void main(String[] args) {
@@ -67,6 +83,7 @@ public class BinarySearch {
         System.out.println("Number of the elements : " +
                 (upperBound - lowerBound)); // output: 3
 
-        System.out.println("lower bound : " + binarySearch(6));
+        System.out.println("lower bound : " + binarySearch(true, 6)); // 4
+        System.out.println("upper bound : " + binarySearch(false, 6)); // 7
     }
 }
